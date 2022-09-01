939b45bf32a3050bab33818d448dc17db61e7a00
Tudor Alexandru <tudorale2005@gmail.com>
2022-09-01 21:15:27 +0300

    updates

--- pages/index.js
+++ pages/index.js
@@ -110,42 +110,6 @@ export default function Home() {
 
       <div className={styles.hero}>
         <h1>Discover The Most Hyped <span>NFT Projects</span></h1>
-
-        <svg
-            className={styles.scroll}
-            width="40"
-            height="77"
-            viewBox="0 0 40 77"
-            >
-              <g id="scrollCircle" transform="translate(-253 -787)">
-                <g
-                  id="Rectangle_12"
-                  transform="translate(253 787)"
-                  fill="none"
-                  stroke="#fff"
-                  strokeWidth="4"
-                >
-                  <rect width="40" height="77" rx="20" stroke="none"></rect>
-                  <rect
-                    x="2"
-                    y="2"
-                    width="36"
-                    height="73"
-                    rx="18"
-                    fill="none"
-                  ></rect>
-                </g>
-                <circle
-                  className={styles.circle}
-                  id="Ellipse_1"
-                  cx="11"
-                  cy="11"
-                  r="11"
-                  transform="translate(262 798)"
-                  fill="#fff"
-                ></circle>
-              </g>
-            </svg>
       </div>
 
       <div>
