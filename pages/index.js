329945a24c160d9cdbc2da3c8eb1903ce30db91d
Tudor Alexandru <tudorale2005@gmail.com>
2022-09-20 15:58:05 +0300

    new tab links

--- pages/index.js
+++ pages/index.js
@@ -214,8 +214,8 @@ export default function Home() {
           <div className={styles.projectName}>
             <p>{popUpName}</p>
             <div className={styles.popUpIcons}>
-              <a href={`${popUpTwitter}`}><img src="/twitter.svg" style={{width: "65px", height: "65px"}}/></a>
-              <a href={`${popUpWebsite}`}>
+              <a href={`${popUpTwitter}`} target="_blank" rel="noreferrer"><img src="/twitter.svg" style={{width: "65px", height: "65px"}}/></a>
+              <a href={`${popUpWebsite}`} target="_blank" rel="noreferrer">
                 <svg xmlns="http://www.w3.org/2000/svg"  width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                               <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                               <circle cx="12" cy="12" r="9" />
@@ -225,7 +225,7 @@ export default function Home() {
                               <path d="M12.5 3a17 17 0 0 1 0 18" />
                             </svg>
               </a>
-              <a href={`${popUpDiscord}`}><img src="/discord.svg"/></a>
+              <a href={`${popUpDiscord}`} target="_blank" rel="noreferrer"><img src="/discord.svg"/></a>
             </div>
 
           </div>
@@ -334,7 +334,7 @@ export default function Home() {
                       </div>
                       <div className={styles.website}>
                         <span className={styles.mobileInfo}>Website:&nbsp;</span>
-                          <a href={`${d.projectWebsiteUrl}`}>
+                          <a href={`${d.projectWebsiteUrl}`} target="_blank" rel="noreferrer">
                           <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
                             <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
                             <circle cx="12" cy="12" r="9" />
@@ -347,13 +347,13 @@ export default function Home() {
                         </div>
                       <div className={styles.twitterNumbers}>
                         <span className={styles.mobileInfo}>Twitter:&nbsp;</span>
-                        <a href={`${d.projectTwitterUrl}`}>
+                        <a href={`${d.projectTwitterUrl}`} target="_blank" rel="noreferrer">
                           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
                         </a>
                       </div>
                       <div className={styles.discordNumbers}>
                         <span className={styles.mobileInfo}>Discord:&nbsp;</span>
-                        <a href={`${d.projectDiscordUrl}`}><img src="/discord.svg"/></a>
+                        <a href={`${d.projectDiscordUrl}`} target="_blank" rel="noreferrer"><img src="/discord.svg"/></a>
                       </div>
                       <div className={styles.daoVotes}>
                         <span className={styles.mobileInfo}>DAO Votes:&nbsp;</span>
