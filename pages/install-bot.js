d6c60852a8453cf38ccf7ddf52919d394fecd4c1
Tudor Alexandru <tudorale2005@gmail.com>
2022-10-01 16:14:22 +0300

    vercel bug

--- pages/install-bot.js
+++ pages/install-bot.js
@@ -1,6 +1,6 @@
 import {useEffect} from "react";
 
-function installbot() {
+function Installbot() {
     useEffect(() =>{
         let a= document.createElement('a');
         a.href="https://discord.com/oauth2/authorize?client_id=994177521614589972&permissions=0&scope=applications.commands%20bot"
@@ -8,4 +8,4 @@ function installbot() {
     }, [])
 }
 
-export default installbot
+export default Installbot
