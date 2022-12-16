0c9f321472120c1f906cda8d4cec0b73e1074a4a
Tudor Alexandru <tudorale2005@gmail.com>
2022-12-16 18:04:46 +0200

    mobile & more blockchains

--- pages/index.js
+++ pages/index.js
@@ -278,19 +278,22 @@ export default function Home() {
   }
 
   const solanaProjects = () => {
-    setSolanaActive(true);
-    setEthActive(false);
-    setAllActive(false);
     let allBtn = document.querySelector(".allProjects");
     let solanaBtn = document.querySelector(".solanaProjects");
     let ethBtn = document.querySelector(".ethProjects");
+    let aptosBtn = document.querySelector(".aptosProjects");
+    let suiBtn = document.querySelector(".suiProjects");
 
     Object.assign(solanaBtn.style, active);
     Object.assign(allBtn.style, unactive);
     Object.assign(ethBtn.style, unactive);
+    Object.assign(aptosBtn.style, unactive);
+    Object.assign(suiBtn.style, unactive);
     solanaBtn.firstChild.style.textShadow = "1px 1px 20px #8900F4"
     allBtn.firstChild.style.textShadow = "none"
     ethBtn.firstChild.style.textShadow = "none"
+    aptosBtn.firstChild.style.textShadow = "none"
+    suiBtn.firstChild.style.textShadow = "none"
 
 
     // filters
@@ -317,21 +320,25 @@ export default function Home() {
   }
 
   const ethProjects = () => {
-    setSolanaActive(false);
-    setEthActive(true);
-    setAllActive(false);
+
     let allBtn = document.querySelector(".allProjects");
     let solanaBtn = document.querySelector(".solanaProjects");
     let ethBtn = document.querySelector(".ethProjects");
+    let aptosBtn = document.querySelector(".aptosProjects");
+    let suiBtn = document.querySelector(".suiProjects");
 
     Object.assign(ethBtn.style, active);
     Object.assign(allBtn.style, unactive);
     Object.assign(solanaBtn.style, unactive);
+    Object.assign(aptosBtn.style, unactive);
+    Object.assign(suiBtn.style, unactive);
     ethBtn.firstChild.style.textShadow = "1px 1px 20px #8900F4"
     allBtn.firstChild.style.textShadow = "none"
     solanaBtn.firstChild.style.textShadow = "none"
-
-     // filters
+    aptosBtn.firstChild.style.textShadow = "none"
+    suiBtn.firstChild.style.textShadow = "none"
+     
+    // filters
      let recentBtn = document.querySelector(".recentButton");
      let rankingBtn = document.querySelector(".rankingButton");
      let launchedBtn = document.querySelector(".launchedButton");
@@ -353,6 +360,91 @@ export default function Home() {
     })
 
   }
+
+  const aptosProjects = () => {
+    
+    let allBtn = document.querySelector(".allProjects");
+    let solanaBtn = document.querySelector(".solanaProjects");
+    let ethBtn = document.querySelector(".ethProjects");
+    let aptosBtn = document.querySelector(".aptosProjects");
+    let suiBtn = document.querySelector(".suiProjects");
+
+    Object.assign(aptosBtn.style, active);
+    Object.assign(allBtn.style, unactive);
+    Object.assign(solanaBtn.style, unactive);
+    Object.assign(ethBtn.style, unactive);
+    Object.assign(suiBtn.style, unactive);
+    aptosBtn.firstChild.style.textShadow = "1px 1px 20px #8900F4"
+    allBtn.firstChild.style.textShadow = "none"
+    solanaBtn.firstChild.style.textShadow = "none"
+    suiBtn.firstChild.style.textShadow = "none"
+    ethBtn.firstChild.style.textShadow = "none"
+
+     // filters
+     let recentBtn = document.querySelector(".recentButton");
+     let rankingBtn = document.querySelector(".rankingButton");
+     let launchedBtn = document.querySelector(".launchedButton");
+ 
+     Object.assign(rankingBtn.style, unactive);
+     Object.assign(recentBtn.style, unactive);
+     Object.assign(launchedBtn.style, unactive);
+     rankingBtn.firstChild.style.textShadow = "none"
+     recentBtn.firstChild.style.textShadow = "none"
+     launchedBtn.firstChild.style.textShadow = "none" 
+    
+    axios.get("https://api.earlylink.io/votes")
+    .then(res => {
+      let newD = res.data.filter((data) => {
+        return data.blockchain == "Aptos";
+        }
+      );
+      setData(newD);
+    })
+
+  }
+
+  const suiProjects = () => {
+
+    let allBtn = document.querySelector(".allProjects");
+    let solanaBtn = document.querySelector(".solanaProjects");
+    let ethBtn = document.querySelector(".ethProjects");
+    let aptosBtn = document.querySelector(".aptosProjects");
+    let suiBtn = document.querySelector(".suiProjects");
+
+    Object.assign(suiBtn.style, active);
+    Object.assign(allBtn.style, unactive);
+    Object.assign(solanaBtn.style, unactive);
+    Object.assign(ethBtn.style, unactive);
+    Object.assign(aptosBtn.style, unactive);
+    suiBtn.firstChild.style.textShadow = "1px 1px 20px #8900F4"
+    allBtn.firstChild.style.textShadow = "none"
+    solanaBtn.firstChild.style.textShadow = "none"
+    aptosBtn.firstChild.style.textShadow = "none"
+    ethBtn.firstChild.style.textShadow = "none"
+
+     // filters
+     let recentBtn = document.querySelector(".recentButton");
+     let rankingBtn = document.querySelector(".rankingButton");
+     let launchedBtn = document.querySelector(".launchedButton");
+ 
+     Object.assign(rankingBtn.style, unactive);
+     Object.assign(recentBtn.style, unactive);
+     Object.assign(launchedBtn.style, unactive);
+     rankingBtn.firstChild.style.textShadow = "none"
+     recentBtn.firstChild.style.textShadow = "none"
+     launchedBtn.firstChild.style.textShadow = "none" 
+    
+    axios.get("https://api.earlylink.io/votes")
+    .then(res => {
+      let newD = res.data.filter((data) => {
+        return data.blockchain == "Aptos";
+        }
+      );
+      setData(newD);
+    })
+
+  }
+
 
   return (
     <>
@@ -434,6 +526,7 @@ export default function Home() {
           <div className={styles.table + " " + "nftsTable"} style={{marginTop: "80px"}}>
             <div className={styles.topPart}>
               <div className={styles.blockchains}>
+
                 <div className={styles.chains + " " + "allProjects"}>
                   <button onClick={allProjects}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
@@ -458,6 +551,18 @@ export default function Home() {
                   <button onClick={ethProjects}>
                     <img src="/ethereum.svg" className={styles.eth}/>
                     ETHEREUM
+                  </button>
+                </div>
+                <div className={styles.chains + " " + "aptosProjects"}>
+                  <button onClick={aptosProjects}>
+                    <img src="/aptos.png" className={styles.aptos}/>
+                    APTOS
+                  </button>
+                </div>
+                <div className={styles.chains + " " + "suiProjects"}>
+                  <button onClick={suiProjects}>
+                    <img src="/sui.png" className={styles.sui}/>
+                    SUI
                   </button>
                 </div>
               </div>
@@ -487,76 +592,81 @@ export default function Home() {
             {
               data ? 
                 data.map((d, index) => (
-                  <div key={d.id}>
-                    <div className={styles.row} onClick={() => PopUpProject(d.projectName, d.projectDescription, d.projectTwitterUrl, d.projectImageUrl, d.upvotes, d.projectDiscordUrl, d.projectWebsiteUrl)} >
-                      <div><span className={styles.mobileInfo}>No.:&nbsp;</span>{index+1}</div>
-                      <div className={styles.name}>
-                        <span className={styles.mobileInfo}>Name:&nbsp;</span>
-                        <img src={`${d.projectLogoUrl}`}/>
-                        <p>{d.projectName}</p>
-                      </div>
-                      <div className={styles.launched}>
-                      <span className={styles.mobileInfo}>Launched:&nbsp;</span>
-                        {d.launched ? 
-                          <p className={styles.launched}>Launched</p>
-                          :
-                          <p className={styles.upcoming}>Upcoming</p>
-                        }
-                      </div>
-                      <div className={styles.projectBlockchain}>
-                        <img src={d.blockchain == "Solana" ? "/solana.svg" : d.blockchain == "Ethereum" ? "/ethereum.svg" : ""} />
-                      </div>
-                      <div className={styles.website}>
-                        <span className={styles.mobileInfo}>Website:&nbsp;</span>
-                          <a href={`${d.projectWebsiteUrl}`} target="_blank" rel="noreferrer">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
-                            <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                            <circle cx="12" cy="12" r="9" />
-                            <line x1="3.6" y1="9" x2="20.4" y2="9" />
-                            <line x1="3.6" y1="15" x2="20.4" y2="15" />
-                            <path d="M11.5 3a17 17 0 0 0 0 18" />
-                            <path d="M12.5 3a17 17 0 0 1 0 18" />
-                          </svg>
+                  <>
+                    <div key={d.id}>
+                      <div className={styles.row} onClick={() => PopUpProject(d.projectName, d.projectDescription, d.projectTwitterUrl, d.projectImageUrl, d.upvotes, d.projectDiscordUrl, d.projectWebsiteUrl)} >
+                        <div><span className={styles.mobileInfo}>No.:&nbsp;</span>{index+1}</div>
+                        <div className={styles.name}>
+                          <span className={styles.mobileInfo}>Name:&nbsp;</span>
+                          <img src={`${d.projectLogoUrl}`}/>
+                          <p>{d.projectName}</p>
+                        </div>
+                        <div className={styles.launched}>
+                        <span className={styles.mobileInfo}>Launched:&nbsp;</span>
+                          {d.launched ? 
+                            <p className={styles.launched}>Launched</p>
+                            :
+                            <p className={styles.upcoming}>Upcoming</p>
+                          }
+                        </div>
+                        <div className={styles.projectBlockchain}>
+                          <span className={styles.mobileInfo}>Blockchain:&nbsp;</span>
+                          <img src={d.blockchain == "Solana" ? "/solana.svg" : d.blockchain == "Ethereum" ? "/ethereum.svg" : ""} />
+                        </div>
+                        <div className={styles.website}>
+                          <span className={styles.mobileInfo}>Website:&nbsp;</span>
+                            <a href={`${d.projectWebsiteUrl}`} target="_blank" rel="noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="44" height="44" viewBox="0 0 24 24" strokeWidth="1.5" stroke="#ffffff" fill="none" strokeLinecap="round" strokeLinejoin="round">
+                              <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
+                              <circle cx="12" cy="12" r="9" />
+                              <line x1="3.6" y1="9" x2="20.4" y2="9" />
+                              <line x1="3.6" y1="15" x2="20.4" y2="15" />
+                              <path d="M11.5 3a17 17 0 0 0 0 18" />
+                              <path d="M12.5 3a17 17 0 0 1 0 18" />
+                            </svg>
+                            </a>
+                          </div>
+                        <div className={styles.twitterNumbers}>
+                          <span className={styles.mobileInfo}>Twitter:&nbsp;</span>
+                          <a href={`${d.projectTwitterUrl}`} target="_blank" rel="noreferrer">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
                           </a>
                         </div>
-                      <div className={styles.twitterNumbers}>
-                        <span className={styles.mobileInfo}>Twitter:&nbsp;</span>
-                        <a href={`${d.projectTwitterUrl}`} target="_blank" rel="noreferrer">
-                          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"/></svg>
-                        </a>
-                      </div>
-                      <div className={styles.discordNumbers}>
-                        <span className={styles.mobileInfo}>Discord:&nbsp;</span>
-                        <a href={`${d.projectDiscordUrl}`} target="_blank" rel="noreferrer"><img src="/discord.svg"/></a>
-                      </div>
-                      <div className={styles.daoVotes}>
-                        <span className={styles.mobileInfo}>DAO Votes:&nbsp;</span>
-                        <p>{d.upvotes}</p>
-                        {d.upvotes >= d.downvotes ? <svg className={styles.up} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <g clipPath="url(#clip0_5_54)">
-                          <path d="M12 4.5L13.7175 6.2175L10.0575 9.8775L7.0575 6.8775L1.5 12.4425L2.5575 13.5L7.0575 9L10.0575 12L14.7825 7.2825L16.5 9V4.5H12Z" fill="#00DC3E"/>
-                          </g>
-                          <defs>
-                          <clipPath id="clip0_5_54">
-                          <rect width="18" height="18" fill="white"/>
-                          </clipPath>
-                          </defs>
-                        </svg> : 
-                        <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                          <g clipPath="url(#clip0_5_109)">
-                          <path d="M12.6667 14.25L14.4796 12.4371L10.6162 8.57375L7.44958 11.7404L1.58333 5.86625L2.69958 4.75L7.44958 9.5L10.6162 6.33333L15.6037 11.3129L17.4167 9.5V14.25H12.6667Z" fill="#DC0000"/>
-                          </g>
-                          <defs>
-                          <clipPath id="clip0_5_109">
-                          <rect width="19" height="19" fill="white"/>
-                          </clipPath>
-                          </defs>
-                        </svg>
-                          
-                        }
+                        <div className={styles.discordNumbers}>
+                          <span className={styles.mobileInfo}>Discord:&nbsp;</span>
+                          <a href={`${d.projectDiscordUrl}`} target="_blank" rel="noreferrer"><img src="/discord.svg"/></a>
+                        </div>
+                        <div className={styles.daoVotes}>
+                          <span className={styles.mobileInfo}>DAO Votes:&nbsp;</span>
+                          <p>{d.upvotes}</p>
+                          {d.upvotes >= d.downvotes ? <svg className={styles.up} width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_5_54)">
+                            <path d="M12 4.5L13.7175 6.2175L10.0575 9.8775L7.0575 6.8775L1.5 12.4425L2.5575 13.5L7.0575 9L10.0575 12L14.7825 7.2825L16.5 9V4.5H12Z" fill="#00DC3E"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_5_54">
+                            <rect width="18" height="18" fill="white"/>
+                            </clipPath>
+                            </defs>
+                          </svg> : 
+                          <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g clipPath="url(#clip0_5_109)">
+                            <path d="M12.6667 14.25L14.4796 12.4371L10.6162 8.57375L7.44958 11.7404L1.58333 5.86625L2.69958 4.75L7.44958 9.5L10.6162 6.33333L15.6037 11.3129L17.4167 9.5V14.25H12.6667Z" fill="#DC0000"/>
+                            </g>
+                            <defs>
+                            <clipPath id="clip0_5_109">
+                            <rect width="19" height="19" fill="white"/>
+                            </clipPath>
+                            </defs>
+                          </svg>
+                            
+                          }
+                        </div>
                       </div>
                     </div>
-                  </div>
+
+                    <div className={styles.mobileLine} key={Math.random() * 888888888.3}></div>
+                  </>
                   )
                 )
 
